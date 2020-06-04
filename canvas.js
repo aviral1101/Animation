96d43ec28f78038b775214b24aed545c5edbd5a8
Aviral <aviral1101@gmail.com>
2020-06-04 21:22:55 +0530

    few changes

--- canvas.js
+++ canvas.js
@@ -16,3 +16,44 @@ c.lineTo(200,200);
 // to color the line
 c.strokeStyle="red";
 c.stroke();
+
+
+
+// lets make an arc
+c.beginPath();
+c.arc(250 ,240 , 30 ,0, Math.PI*2,false);
+c.stroke();
+
+
+// function Circle(x,y,dx,dy,radius){
+//   this.x=x;
+//   this.y=y;
+//   this.draw=function(){
+//
+//   }
+// }
+
+
+/// making a bouncing circle
+var x=Math.random()*innerWidth;
+var y=Math.random()*innerHeight;
+var dx=Math.random()-0.5;
+var dy=Math.random()-0.5;
+var radius =30;
+function animate(){
+  requestAnimationFrame(animate);
+  c.clearRect(0,0,innerWidth,innerHeight);
+  c.beginPath();
+  c.arc(x,y,radius,0,Math.PI*2,false);
+  c.strokeStyle="blue";
+  c.storke();
+  if (x+radius>innerWidth || x-radius<0){
+    dx=-dx;
+  }
+  if (y+radius>innerWidth||y-radius<0){
+    dy=-dy;
+  }
+  x+=dx;
+  y+=dy;
+
+}
