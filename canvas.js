8d1335e3dd6f0a0f164b69c4267cadc088b0bffd
Aviral <aviral1101@gmail.com>
2020-06-04 17:35:16 +0530

    change-to-js

---
+++ canvas.js
@@ -0,0 +1,6 @@
+var canvas=document.queryselector("canvas");
+canvas.width = windows.innerWidth;
+canvas.height = windows.innerHeight;
+// to draw the canvas
+var c= canvas.getContext("2d");
+c.fillRect(40 ,50 ,100 ,100);
