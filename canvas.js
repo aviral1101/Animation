f6fb48e7cf22edc41fb41867d41def7648a73a89
Aviral <aviral1101@gmail.com>
2020-06-04 18:06:07 +0530

    changes-js

--- canvas.js
+++ canvas.js
@@ -3,4 +3,16 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 // to draw the canvas
 var c= canvas.getContext("2d");
+c.fillStyle="lightpink";
 c.fillRect(40 ,50 ,100 ,100);
+
+
+
+
+// lets draw a line
+c.beginPath();
+c.moveTo(300,50);
+c.lineTo(200,200);
+// to color the line
+c.strokeStyle="red";
+c.stroke();
